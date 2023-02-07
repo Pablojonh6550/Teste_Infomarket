dcb1179556bb3454fbb1a54b0ddf7e3dcdedae27
Pablojonh6550 <pablojonh6550@gmail.com>
2023-02-07 19:41:01 -0300

    chove: configuração banco de dados

---
+++ server/db.js
@@ -0,0 +1,19 @@
+// conexão com o banco
+async function connect() {
+  if (global.connection && global.connection.state !== "disconnected")
+    return global.connection;
+
+  const mysql = require("mysql2/promise");
+  const connection = await mysql.createConnection(
+    "mysql://root@localhost:3306/teste_infomarket"
+  );
+
+  global.connection = connection;
+  console.log("Connection Success");
+
+  return connection;
+}
+
+connect();
+
+module.exports = { connect };
