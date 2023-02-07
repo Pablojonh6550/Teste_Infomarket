342bf454899d1b1d731ca970784b62c915a35544
Pablojonh6550 <pablojonh6550@gmail.com>
2023-02-07 20:52:16 -0300

    feat: global import

--- server/db.js
+++ server/db.js
@@ -1,5 +1,6 @@
 // conexão com o banco
 async function connect() {
+  // verificação de conexão
   if (global.connection && global.connection.state !== "disconnected")
     return global.connection;
 
@@ -8,12 +9,11 @@ async function connect() {
     "mysql://root@localhost:3306/teste_infomarket"
   );
 
+  // variável global de conexão com o banco
   global.connection = connection;
   console.log("Connection Success");
 
   return connection;
 }
-
-connect();
 
 module.exports = { connect };
