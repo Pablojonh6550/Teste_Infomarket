1863021a4dfcdb2baaf79058a0e3bbedc4f71f74
Pablojonh6550 <pablojonh6550@gmail.com>
2023-02-07 20:49:54 -0300

    feat: inserção de dados

--- server/controllers.js
+++ server/controllers.js
@@ -1,9 +1,6 @@
-const connect = require("./db");
-console.log("foi");
-
 // carrega todas as pessoas presentes no banco
 async function selectAllPeople() {
-  const conn = await connect.connect();
+  const conn = global.connection;
   const [rows] = await conn.query(
     "SELECT * FROM pessoa INNER JOIN endereco ON pessoa.endereco_id = endereco.id;"
   );
@@ -12,24 +9,25 @@ async function selectAllPeople() {
 
 // insere o endereço da pessoa no banco
 async function insertAddress(request) {
-  const conn = await connect.connect();
+  const conn = global.connection;
 
   const sql =
     "INSERT INTO endereco(rua, numero, bairro, cep) VALUES (?,?,?,?);";
   const values = [request.rua, request.numero, request.bairro, request.cep];
-  const query = await conn.query(sql, values);
+  const query = await conn.query(sql, values).then((result) => {
+    return result;
+  });
 
-  const result = await query.then((result) => result.data);
-  console.log(result);
+  return query[0].insertId;
 }
 
 // insere os dados da pessoa no banco
 async function insertPeople(request) {
-  const adress = insertAddress();
-  const conn = await connect.connect();
+  const addressId = await insertAddress(request.address);
+  const conn = global.connection;
 
   const sql = "INSERT INTO pessoa(nome, endereco_id, idade) VALUES (?,?,?)";
-  const values = [request.nome, , request.idade];
+  const values = [request.nome, addressId, request.idade];
 
   await conn.query(sql, values);
 }
