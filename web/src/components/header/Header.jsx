f8fddf87632d065047aeea1177b2dc576689ee97
Pablojonh6550 <pablojonh6550@gmail.com>
2023-02-09 09:33:15 -0300

    feat: cabeçalho

--- web/src/components/header/Header.jsx
+++ web/src/components/header/Header.jsx
@@ -1,11 +1,12 @@
-import Button from "../button/Button";
+import ModalForm from "../form/ModalForm";
 
 export default function Header() {
   return (
     <div className="row justify-content-center">
       <div className="col-md-8 d-flex justify-content-around mb-5">
         <h2 className="heading-section">Tabela Pessoas</h2>
-        <Button title={"Cadastrar"} />
+
+        <ModalForm nameButton={"Cadastrar"} />
       </div>
     </div>
   );
